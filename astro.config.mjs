ac0198aa647e6b2fe21ebbb653666a098b8d1162
Jesus Hernandez <jesusbeckan@gmail.com>
2025-01-16 01:23:31 -0500

    Adds some tweaks Updates copies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  env: {
+    schema: {
+      SPEAKER_FORM_URL: envField.string({ context: "client", access: "public", optional: true }),
+      BUY_TICKET_URL: envField.string({ context: "client", access: "public", optional: true })
+    }
+  }
 });
