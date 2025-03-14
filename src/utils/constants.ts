74fbf3df5143544a7fa0caa824da18c1ee69da20
Jesus Hernandez <jesusbeckan@gmail.com>
2025-03-14 18:53:34 -0500

    Implements sponsors and speakers sections

---
+++ src/utils/constants.ts
@@ -0,0 +1,67 @@
+export const speakers = [
+  {
+    name: 'Ana Rangel',
+    role: 'UX/UI Designer',
+    country: 'venezuela',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741666982/ana-rangel_txu6bz.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668785/mini-ana-rangel_wn1578.webp'
+  },
+  {
+    name: 'Daniel Minor',
+    role: 'SpiderMonkey Engineer At Mozilla',
+    country: 'canada',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741877390/daniel-minor_vvuimm.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741877390/mini-daniel-minor_wx9hwc.webp'
+  },
+  {
+    name: 'Francisca Medina',
+    role: 'Frontend Team Lead',
+    country: 'chile',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741738174/francisca-medina_e1qh7e.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741738045/mini-francisca-medina_qnfzxt.webp'
+  }
+]
+
+
+export const emeraldSponsors = [
+  {
+    image: '/images/epam-logo.webp',
+    url: 'https://www.epam.com/',
+    alt: 'Logo de EPAM'
+  }
+]
+
+export const pearlSponsors = []
+
+export const quartzSponsors = []
+
+export const allySponsors = [
+  {
+    image: '/images/fomo-logo.webp',
+    url: 'https://joinfomo.co/',
+    alt: 'Logo de Fomo',
+    width: 70,
+    height: 70
+  },
+  {
+    image: '/images/blazing-logo.webp',
+    url: 'https://blazingsoft.co/',
+    alt: 'Logo de Blazing',
+    width: 150,
+    height: 150
+  },
+  {
+    image: '/images/notion-logo.webp',
+    url: 'https://www.notion.com/',
+    alt: 'Logo de Notion',
+    width: 70,
+    height: 70
+  },
+  {
+    image: '/images/jetbrains-logo.webp',
+    url: 'https://www.jetbrains.com/',
+    alt: 'Logo de JetBrains',
+    width: 150,
+    height: 150
+  }
+]
