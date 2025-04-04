6c157a72fd74734ddd3c2672abb57cdc88943d45
Jesus Hernandez <jesusbeckan@gmail.com>
2025-04-03 22:21:04 -0500

    feat: adds new speakers and sponsors

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,8 +5,8 @@ export const speakers = [
     country: 'venezuela',
     image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741666982/ana-rangel_txu6bz.webp',
     thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668785/mini-ana-rangel_wn1578.webp',
-    talk: "Diseño vs. Devs: Como ese roommate que nunca limpia",
-    schedule: "09:00AM - 09:40AM",
+    talk: 'Diseño vs. Devs: Como ese roommate que nunca limpia',
+    schedule: '09:00AM - 09:40AM',
   },
   {
     name: 'Daniel Minor',
@@ -14,8 +14,8 @@ export const speakers = [
     country: 'canada',
     image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743430583/daniel-minor_qd7qpd.webp',
     thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743430585/mini-daniel-minor_naz32i.webp',
-    talk: "La evolución de JavaScript: Desde la idea a la implementación",
-    schedule: "10:00AM - 10:40AM",
+    talk: 'La evolución de JavaScript: Desde la idea a la implementación',
+    schedule: '09:45AM - 10:05AM',
   },
   {
     name: 'Francisca Medina',
@@ -23,8 +23,8 @@ export const speakers = [
     country: 'chile',
     image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741738174/francisca-medina_e1qh7e.webp',
     thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741738045/mini-francisca-medina_qnfzxt.webp',
-    talk: "La A en el STEAM: Experimentación artística con algoritmos",
-    schedule: "10:45AM - 11:25AM",
+    talk: 'La A en el STEAM: Experimentación artística con algoritmos',
+    schedule: '10:30AM - 10:50AM',
   },
   {
     name: 'Ana Barragan',
@@ -32,8 +32,26 @@ export const speakers = [
     country: 'colombia',
     image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1742332963/ana-barragan_xatoej.webp',
     thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1742332963/mini-ana-barragan_yvd8pb.webp',
-    talk: "IA generativa práctica usando Amazon Nova",
-    schedule: "11:30AM - 12:00PM",
+    talk: 'IA generativa práctica usando Amazon Nova',
+    schedule: '10:55AM - 11:35pM',
+  },
+  {
+    name: 'Rina Plata',
+    role: 'Frontend Developer',
+    country: 'colombia',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743728255/rina-plata_i7wtxy.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743728255/mini-rina-plata_tj9gaf.webp',
+    talk: 'Accesibilidad, inclusióndesde tu Código ',
+    schedule: '11:40AM - 12:00PM',
+  },
+  {
+    name: 'Angie Zuñiga',
+    role: 'Software Engineer',
+    country: 'colombia',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743736175/angie-zun%CC%83iga_goz7mn.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743736175/mini-angie-zun%CC%83iga_s9x6oc.webp',
+    talk: 'Generando data sintética: Usos y Retos',
+    schedule: '01:00PM - 01:40PM',
   },
   {
     name: 'Erasmo Hernández',
@@ -41,17 +59,8 @@ export const speakers = [
     country: 'venezuela',
     image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1742144209/mini-erasmo-hernandez_t1frpz.webp',
     thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1742333169/mini-erasmo-hernandez_bzwhko.webp',
-    talk: "De Text Editor a la IA: Evolución de los editores de código moderno",
-    schedule: "09:00AM - 09:40AM",
-  },
-  {
-    name: 'Juan Guillermo',
-    role: 'Software Engineer',
-    country: 'colombia',
-    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743429804/juan-guillermo_iadmpo.webp',
-    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743429804/mini-juan-guillermo_ce8bj6.webp',
-    talk: "Creating a RAG with Bigquery and Gemini and Python",
-    schedule: "09:00AM - 09:40AM",
+    talk: 'De Text Editor a la IA: Evolución de los editores de código moderno',
+    schedule: '01:45PM - 02:05PM',
   },
   {
     name: 'Miguel Teheran',
@@ -59,9 +68,72 @@ export const speakers = [
     country: 'colombia',
     image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743430393/miguel-teheran_f9t0ug.webp',
     thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743430392/mini-miguel-teheran_hwcstc.webp',
-    talk: "No es un bug, es un daño colateral: hablemos de programación, ética y responsabilidad social.",
-    schedule: "09:00AM - 09:40AM",
-  }
+    talk: 'No es un bug, es un daño colateral: Programación, ética y responsabilidad social.',
+    schedule: '02:30PM - 03:10PM',
+  },
+  {
+    name: 'Jesus Rincon',
+    role: 'Data Engineer',
+    country: 'colombia',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743735878/jesus-rincon_fw0usg.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743735878/mini-jesus-rincon_fzxhw3.webp',
+    talk: 'From Qubits to Machine Learning: Understanding Quantum Innovation',
+    schedule: '03:15PM - 03:55PM',
+  },
+  {
+    name: 'Por Confirmar',
+    role: 'Por Confirmar',
+    country: 'Por Confirmar',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668329/speaker_gndva4.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668329/speaker_gndva4.webp',
+    talk: 'Por Confirmar',
+    schedule: '04:00PM - 04:40PM',
+  },
+  {
+    name: 'Sary Libreros',
+    role: 'Product Manager',
+    country: 'colombia',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743555956/sary-libreros_lbrgt7.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743555955/mini-sary-libreros_ngm8bv.webp',
+    talk: 'Construye productos inteligentes con la fuerza de Amazon Bedrock',
+    schedule: '01:00PM - 01:40PM',
+  },
+  {
+    name: 'Por Confirmar',
+    role: 'Por Confirmar',
+    country: 'Por Confirmar',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668329/speaker_gndva4.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668329/speaker_gndva4.webp',
+    talk: 'Por Confirmar',
+    schedule: '01:45PM - 02:05PM',
+  },
+  {
+    name: 'Juan Gómez',
+    role: 'Software Engineer',
+    country: 'colombia',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743429804/juan-guillermo_iadmpo.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1743429804/mini-juan-guillermo_ce8bj6.webp',
+    talk: 'Creating a RAG with Bigquery and Gemini and Python',
+    schedule: '02:30PM - 03:10PM',
+  },
+  {
+    name: 'Luis Robles',
+    role: 'Solutions Architect',
+    country: 'colombia',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668329/speaker_gndva4.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668329/speaker_gndva4.webp',
+    talk: 'Programación de Bajo Nivel y uso eficiente de la memoria',
+    schedule: '03:15PM - 03:55PM',
+  },
+  {
+    name: 'Por Confirmar',
+    role: 'Por Confirmar',
+    country: 'Por Confirmar',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668329/speaker_gndva4.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668329/speaker_gndva4.webp',
+    talk: 'Por Confirmar',
+    schedule: '04:00PM - 04:40PM',
+  },
 ]
 
 
@@ -90,15 +162,15 @@ export const quartzSponsors = [
     image: '/images/avvy-logo.webp',
     url: 'https://www.avvy.co/',
     alt: 'Logo de Avvy',
-    width: 200,
-    height: 200
+    width: 180,
+    height: 180
   },
   {
     image: '/images/miguel-reheran.webp',
     url: 'https://mteheran.dev',
     alt: 'Logo de Avvy',
-    width: 200,
-    height: 200
+    width: 180,
+    height: 180
   }
 ]
 
@@ -151,5 +223,12 @@ export const allySponsors = [
     alt: 'Logo de JetBrains',
     width: 150,
     height: 150
+  },
+  {
+    image: '/images/logo-bdg-Institute.png',
+    url: 'https://bdginstitute.edu.co/',
+    alt: 'Logo de BDG Institute',
+    width: 120,
+    height: 120
   }
 ]
