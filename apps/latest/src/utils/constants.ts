534fae8a3e6980d080196d3317a52b52f488a110
Jesus Hernandez <jesusbeckan@gmail.com>
2026-06-11 22:43:36 -0500

    feat(speakers): redesigns section with interactive showcase display

--- apps/latest/src/utils/constants.ts
+++ apps/latest/src/utils/constants.ts
@@ -1,2 +1,24 @@
 export const SPEAKER_FORM_URL = import.meta.env.SPEAKER_FORM_URL;
 export const BUY_TICKET_URL = import.meta.env.BUY_TICKET_URL;
+
+export const speakers = [
+  {
+    name: 'Ana Rangel',
+    role: 'UX/UI Designer',
+    country: 'venezuela',
+    image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741666982/ana-rangel_txu6bz.webp',
+    thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668785/mini-ana-rangel_wn1578.webp',
+    talk: 'Diseño vs. Devs: Como ese roommate que nunca limpia',
+    schedule: '09:00AM - 09:40AM',
+  }
+]
+
+
+export const speakerToBeConfirmed = {
+  name: 'Por Confirmar',
+  role: ' ',
+  country: '',
+  thumbnail: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668329/speaker_gndva4.webp',
+  image: 'https://res.cloudinary.com/dpu0uajtw/image/upload/v1741668329/speaker_gndva4.webp',
+  alt: 'Imagen de conferencista por confirmar'
+}
